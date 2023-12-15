f88e86118b7c7aee172718a12a837927548bf3e5
amyyyyyyyyyyyyyyyyyy <153893416+amyyyyyyyyyyyyyyyyyy@users.noreply.github.com>
2023-12-15 14:48:06 +0300

    Add files via upload
    Uploaded my introduction to python from Strathmore coding camp

---
+++ Amy's python code '23/idk.py
@@ -0,0 +1,16 @@
+def multiplynumbers(a,b):
+    result= a*b
+    return result
+
+def multiply():
+    a=20
+    b=10
+    result=a*b
+    return result
+
+def multiplynum():
+    a=20
+    b=10
+    result=a*b
+    print(result)
+    
